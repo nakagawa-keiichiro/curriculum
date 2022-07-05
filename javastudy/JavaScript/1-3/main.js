ca681d792948c73007f8a1162912cad7f606ee50
nakagawa-keiichiro <102967646+nakagawa-keiichiro@users.noreply.github.com>
2022-07-05 21:12:42 +0900

    課題提出テスト
    1-3の修正になります。

--- javastudy/JavaScript/1-3/main.js
+++ javastudy/JavaScript/1-3/main.js
@@ -2,12 +2,15 @@ let a= 1;
 let b = 14;
 
 //問1
-console.log(a === 10);
+//aは10ではない
+console.log(a !== 10); 
+
+//bは10以上20未満、かつ偶数である。
 console.log(b >= 10 && b <20 && b %2 === 0 );
 
 //問２
 let x = 10;
-if (x >= 10 && x <20) {
+if (x >= 10 && x <=20) {
     console.log('成功です');
 } else {
     console.log('失敗です'); 
@@ -15,7 +18,7 @@ if (x >= 10 && x <20) {
 
 //問３
 let y = 0;
-if (x % 2 === 0 ) {
+if (y % 2 === 0 ) {
     console.log('偶数です');
 } else {
     console.log('奇数です'); 
