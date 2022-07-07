1a3f06e919163225e32d51fa286c9c358c469638
nakagawa-keiichiro <102967646+nakagawa-keiichiro@users.noreply.github.com>
2022-07-07 21:26:40 +0900

    課題提出テスト
    1－7修正

--- javastudy/JavaScript/1-7/main.js
+++ javastudy/JavaScript/1-7/main.js
@@ -2,25 +2,19 @@
 class Taiyaki {
 
     //コンストラクタ
-    constructor(anko,krim,tizu) {
-        this.anko = anko;
-        this.krim = krim;
-        this.tizu = tizu;
+    constructor(nakami) {
+        this.nakami = nakami;
     }
 
     //メソッド（関数）作成
-    nakami() {
-        console.log(`中身は${this.anko}です`);
-    }
     nakami1() {
-        console.log(`中身は${this.krim}です`);
-    }
-    nakami2() {
-        console.log(`中身は${this.tizu}です`);
+        console.log(`中身は${this.nakami}です`);
     }
 }
 //オブジェクト作成
-let yamada = new Taiyaki('あんこ','クリーム','チーズ');
-yamada.nakami();
+let yamada = new Taiyaki('あんこ');
 yamada.nakami1();
-yamada.nakami2();
+let tanaka = new Taiyaki('クリーム');
+tanaka.nakami1();
+let saito = new Taiyaki('チーズ');
+saito.nakami1();
