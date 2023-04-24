e137b0a18c81788a36f36922c4c9e9b6548f5e73
nakagawa-keiichiro <102967646+nakagawa-keiichiro@users.noreply.github.com>
2023-04-24 19:53:23 +0900

    課題提出テスト
    Main.javaの修正

--- javajob/3-1/Main.java
+++ javajob/3-1/Main.java
@@ -19,7 +19,7 @@ public class Main{
         }
         System.out.println();
         for (int i = 0; i < data.length; i++) {
-            for (int j = 4; j > i; j--) {
+            for (int j = data.length-1; j > i; j--) {
                 /*
                 * 問3
                 * 以下、配列の添字を入れてソートを完成させなさい
